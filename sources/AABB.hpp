32495b2e354ab966fe80c39126fec08f5dc49346
Andrey Sheveliov <adron1990@vk.com>
2026-04-05 02:04:06 +0300

    small refactor aabb

--- sources/AABB.hpp
+++ sources/AABB.hpp
@@ -1,10 +1,44 @@
 #pragma once
 
+#include "components/Transform.hpp"
 #include <cmath>
 #include <glm/glm.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/euler_angles.hpp>
 
 struct AABB {
-    glm::vec3 min{+INFINITY};
-    glm::vec3 max{-INFINITY};
+    glm::vec3 min { +INFINITY };
+    glm::vec3 max { -INFINITY };
 };
 
+inline AABB toGlobalAABB(const AABB& aabb, const Transform& transform) noexcept
+{
+    auto model = transform.getMatrix();
+
+    // auto model = glm::mat4 { 1.0f };
+    // model = glm::rotate(model, glm::radians(transform.rotation.x), { 1.0f, 0.0f, 0.0f });
+    // model = glm::rotate(model, glm::radians(transform.rotation.y), { 0.0f, 1.0f, 0.0f });
+    // model = glm::rotate(model, glm::radians(transform.rotation.z), { 0.0f, 0.0f, 1.0f });
+
+    glm::vec3 corners[8] = {
+        { aabb.min.x, aabb.min.y, aabb.min.z },
+        { aabb.max.x, aabb.min.y, aabb.min.z },
+        { aabb.min.x, aabb.max.y, aabb.min.z },
+        { aabb.max.x, aabb.max.y, aabb.min.z },
+        { aabb.min.x, aabb.min.y, aabb.max.z },
+        { aabb.max.x, aabb.min.y, aabb.max.z },
+        { aabb.min.x, aabb.max.y, aabb.max.z },
+        { aabb.max.x, aabb.max.y, aabb.max.z }
+    };
+
+    glm::vec3 worldMin { +FLT_MAX };
+    glm::vec3 worldMax { -FLT_MAX };
+
+    for (int i = 0; i < 8; i++) {
+        glm::vec3 transformed = glm::vec3(model * glm::vec4(corners[i], 1.0f));
+        worldMin = glm::min(worldMin, transformed);
+        worldMax = glm::max(worldMax, transformed);
+    }
+
+    return AABB { worldMin, worldMax };
+}
